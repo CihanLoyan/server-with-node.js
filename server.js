f75b4d618c9b80a475b0717ac8b1ecc47bdfe8b1
cloyan <cloyan@ebay.com>
2022-01-12 14:52:59 +0300

    routing to pages requests to the server

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     res.setHeader('Content-Type', 'text/html')
-    
 
-    res.end()
+
+    let path = './views/'
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break
+        case '/about':
+            path += 'about.html'
+            break
+        default:
+            path += '404.html'
+            break
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end()
+        }else{
+            res.write(data)
+            res.end()
+        }
+    }) 
 })
 
 server.listen(3000, 'localhost', () => {
