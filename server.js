f5b5b70a19617c49017c461627a00cbb758f6ad4
cloyan <cloyan@ebay.com>
2022-01-12 16:44:06 +0300

    statusCode used

--- server.js
+++ server.js
@@ -11,12 +11,19 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             path += 'index.html'
+            res.statusCode = 200
             break
         case '/about':
             path += 'about.html'
+            res.statusCode = 200
+            break
+        case '/favicon.ico':
+            res.statusCode = 301
+            res.setHeader('Location', 'index.html')
             break
         default:
             path += '404.html'
+            res.statusCode = 404
             break
     }
 
